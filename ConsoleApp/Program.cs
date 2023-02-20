89589aab8886966f0bfac26d28680e9e5b38d975
Romain Lutigneaux <rlutigneaux@cesi.fr>
2023-02-20 12:16:24 +0100

    feat(main): check integer

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -8,19 +8,30 @@ class Program
 
     static void Main(string[] args)
     {
-        //int salary = int.Parse(args[0]);
-        //int tax = int.Parse(args[1]);
         Console.OutputEncoding = System.Text.Encoding.UTF8;
         Console.WriteLine("Enter your salary before tax: ");
-        int salary = int.Parse(Console.ReadLine());
-        Console.WriteLine("Enter the value of tax: ");
-        int tax = int.Parse(Console.ReadLine());
+        bool salaryIsInt = int.TryParse(Console.ReadLine(), out int salary);
 
-        int netSalary = CalculateSalary(salary, tax);
+        if (salaryIsInt)
+        {
+            Console.WriteLine("Enter the value of tax: ");
+            bool taxIsInt = int.TryParse(Console.ReadLine(), out int tax);
+            if (taxIsInt)
+            {
+                int netSalary = CalculateSalary(salary, tax);
 
-        Console.WriteLine("Si votre salaire est de " + salary + "€ et que votre taux d'imposition " +
-            "est de " + tax + "% alors votre net mensuel est de : " + netSalary + "€");
-
+                Console.WriteLine("Si votre salaire est de " + salary + "€ et que votre taux d'imposition " +
+                    "est de " + tax + "% alors votre net mensuel est de : " + netSalary + "€");
+            }
+            else
+            {
+                Console.WriteLine("Sorry but your tax it's not an integer");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Sorry but your salary it's not an integer");
+        }
         Console.ReadLine();
     }
 }
